bd0bdfe988b07091078e28c6a072a7745bc8e201
franck <carnet.franck.paul@gmail.com>
2023-05-22 16:13:19 +0200

    Minify fix

--- _define.php
+++ _define.php
@@ -13,7 +13,7 @@ $this->registerModule(
     'ErrorLogger',
     'Error logger for Dotclear2',
     'Bruno Hondelatte',
-    '2.1',
+    '2.1.1',
     [
         'type'        => 'plugin',
         'requires'    => [['core', '2.26']],

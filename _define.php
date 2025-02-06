ffbaa1ab54a5eb2f9c219a7089b87624a55e7eb8
franck <carnet.franck.paul@gmail.com>
2025-02-06 12:28:37 +0100

    New release 5.1

--- _define.php
+++ _define.php
@@ -14,9 +14,9 @@ $this->registerModule(
     'ErrorLogger',
     'Error logger for Dotclear2',
     'Bruno Hondelatte',
-    '5.0',
+    '5.1',
     [
-        'date'        => '2003-08-13T13:42:00+0100',
+        'date'        => '2025-02-06T12:28:37+0100',
         'type'        => 'plugin',
         'requires'    => [['core', '2.33']],
         'permissions' => 'My',

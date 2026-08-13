4b49ff6c4c48ad2b5f930d03856af29612a147cc
franck <contact@open-time.net>
2026-08-13 16:40:32 +0200

    Enforces some verification before registering module

--- _define.php
+++ _define.php
@@ -10,22 +10,26 @@
  *
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-$this->registerModule(
-    'ErrorLogger',
-    'Error logger for Dotclear2',
-    'Bruno Hondelatte',
-    '8.0',
-    [
-        'date'        => '2026-08-03T09:55:07+0200',
-        'type'        => 'plugin',
-        'requires'    => [['core', '2.39']],
-        'permissions' => 'My',
+declare(strict_types=1);
 
-        'priority' => 1,
+if (isset($this) && is_object($this) && method_exists($this, 'registerModule') && isset($this->id) && is_string($this->id)) {
+    $this->registerModule(
+        'ErrorLogger',
+        'Error logger for Dotclear2',
+        'Bruno Hondelatte',
+        '8.0',
+        [
+            'date'        => '2026-08-03T09:55:07+0200',
+            'type'        => 'plugin',
+            'requires'    => [['core', '2.39']],
+            'permissions' => 'My',
 
-        'details'    => 'https://open-time.net/?q=errorlogger',
-        'support'    => 'https://github.com/franck-paul/errorlogger',
-        'repository' => 'https://raw.githubusercontent.com/franck-paul/errorlogger/main/dcstore.xml',
-        'license'    => 'gpl2',
-    ]
-);
+            'priority' => 1,
+
+            'details'    => 'https://open-time.net/?q=errorlogger',
+            'support'    => 'https://github.com/franck-paul/errorlogger',
+            'repository' => 'https://raw.githubusercontent.com/franck-paul/errorlogger/main/dcstore.xml',
+            'license'    => 'gpl2',
+        ]
+    );
+}
